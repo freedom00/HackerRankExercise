8cf717587cce4e9cdd17dff7126c987e34dbe8a3
freedom00 <jonathan888999@163.com>
2024-06-16 01:32:44 -0400

    solve me first

---
+++ src/main/java/com/hackerrank/problemsolving/easy/Solution.java
@@ -0,0 +1,8 @@
+package com.hackerrank.problemsolving.easy;
+
+public class Solution {
+    //1. Solve Me First
+    public static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/hackerrank/problemsolving/easy/SolutionTest.java
@@ -0,0 +1,12 @@
+package com.hackerrank.problemsolving.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void solveMeFirst() {
+        assertEquals(4, Solution.solveMeFirst(2, 2));
+    }
+}
