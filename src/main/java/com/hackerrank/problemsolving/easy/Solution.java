166795fc97c37993ea36ea28b90ec65a06e8f173
freedom00 <jonathan888999@163.com>
2024-06-16 01:41:08 -0400

    simple array sum

--- src/main/java/com/hackerrank/problemsolving/easy/Solution.java
+++ src/main/java/com/hackerrank/problemsolving/easy/Solution.java
@@ -1,8 +1,15 @@
 package com.hackerrank.problemsolving.easy;
 
+import java.util.List;
+
 public class Solution {
     //1. Solve Me First
     public static int solveMeFirst(int a, int b) {
         return a + b;
+    }
+
+    //2. Simple Array Sum
+    public static int simpleArraySum(List<Integer> ar) {
+        return ar.stream().mapToInt(i -> i).sum();
     }
 }
--- src/test/java/com/hackerrank/problemsolving/easy/SolutionTest.java
+++ src/test/java/com/hackerrank/problemsolving/easy/SolutionTest.java
@@ -2,11 +2,18 @@ package com.hackerrank.problemsolving.easy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolutionTest {
     @Test
     public void solveMeFirst() {
         assertEquals(4, Solution.solveMeFirst(2, 2));
+    }
+
+    @Test
+    void simpleArraySum() {
+        assertEquals(31, Solution.simpleArraySum(Arrays.asList(1, 2, 3, 4, 10, 11)));
     }
 }
