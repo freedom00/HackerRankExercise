94d6159c6f91cfaeed0f847b5badb7e1392c9646
freedom00 <jonathan888999@163.com>
2024-06-16 10:23:24 -0400

    compare the triplets

--- src/main/java/com/hackerrank/problemsolving/easy/Solution.java
+++ src/main/java/com/hackerrank/problemsolving/easy/Solution.java
@@ -1,5 +1,6 @@
 package com.hackerrank.problemsolving.easy;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
@@ -11,5 +12,19 @@ public class Solution {
     //2. Simple Array Sum
     public static int simpleArraySum(List<Integer> ar) {
         return ar.stream().mapToInt(i -> i).sum();
+    }
+
+    //3. Compare the Triplets
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int sumA = 0;
+        int sumB = 0;
+        for (int i = 0; i < Math.min(a.size(), b.size()); i++) {
+            if (a.get(i) < b.get(i)) {
+                sumB++;
+            } else if (a.get(i) > b.get(i)) {
+                sumA++;
+            }
+        }
+        return Arrays.asList(sumA, sumB);
     }
 }
--- src/test/java/com/hackerrank/problemsolving/easy/SolutionTest.java
+++ src/test/java/com/hackerrank/problemsolving/easy/SolutionTest.java
@@ -3,6 +3,7 @@ package com.hackerrank.problemsolving.easy;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,5 +16,12 @@ public class SolutionTest {
     @Test
     void simpleArraySum() {
         assertEquals(31, Solution.simpleArraySum(Arrays.asList(1, 2, 3, 4, 10, 11)));
+    }
+
+    @Test
+    void compareTriplets() {
+        List<Integer> a = Arrays.asList(5, 6, 7);
+        List<Integer> b = Arrays.asList(3, 6, 10);
+        assertEquals(Arrays.asList(1, 1), Solution.compareTriplets(a, b));
     }
 }
